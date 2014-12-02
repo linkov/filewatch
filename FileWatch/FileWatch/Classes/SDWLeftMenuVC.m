e973c78935483560d2a087df0d4bc08358ecb550
Alex Linkov <alexey.linkov@gmail.com>
2014-12-03 01:56:47 +0300

    setup tableview sections

--- FileWatch/FileWatch/Classes/SDWLeftMenuVC.m
+++ FileWatch/FileWatch/Classes/SDWLeftMenuVC.m
@@ -7,11 +7,17 @@
 //
 #import "SDWRecipe.h"
 #import "SDWLeftMenuVC.h"
+#import "JWCTableView.h"
 
-@interface SDWLeftMenuVC () <NSTableViewDataSource, NSTableViewDelegate>
-@property (strong) IBOutlet NSTableView *tableView;
-@property (strong) NSArray *items;
+@interface SDWLeftMenuVC () <JWCTableViewDataSource, JWCTableViewDelegate>
+@property (strong) IBOutlet JWCTableView *tableView;
 
+//@property (strong) NSArray *items;
+@property (strong) NSArray *sections;
+@property (strong) NSDictionary *content;
+
+@property (nonatomic, retain) NSMutableArray *sectionKeys;
+@property (nonatomic, retain) NSMutableDictionary *sectionContents;
 
 @end
 
@@ -20,11 +26,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.items = [self seededRecipies];
+  //  self.items = [self seededRecipies];
+//    self.sections = @[@"Programming",@"General"];
+//    self.content = @{self.sections[0]:[self seededRecipies],self.sections[1]:[self seededRecipiesGeneral]};
+
+    NSMutableArray *keys = [[NSMutableArray alloc] init];
+    NSMutableDictionary *contents = [[NSMutableDictionary alloc] init];
+
+    NSString *general = @"General";
+    NSString *programming = @"Programming";
+
+    [contents setObject:[self seededRecipiesGeneral] forKey:general];
+    [contents setObject:[self seededRecipies] forKey:programming];
+
+    [keys addObject:general];
+    [keys addObject:programming];
+
+
+    self.sectionKeys = keys;
+    self.sectionContents = contents;
+
 }
 
-- (NSArray *)seededRecipies {
-
+- (NSArray *)seededRecipiesGeneral {
 
     SDWRecipe *curlies = [SDWRecipe new];
     curlies.name = @"Curly brackets should start on the same line as method name";
@@ -38,50 +62,95 @@
     return recipes;
 }
 
-#pragma mark - NSTableViewDataSource, NSTableViewDelegate
+- (NSArray *)seededRecipies {
 
-- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
-    return self.items.count;
+    SDWRecipe *curlies = [SDWRecipe new];
+    curlies.name = @"Curly brackets should start on the same line as method name";
+    curlies.offLimit = 1;
+    curlies.amberLimit = 3;
+    curlies.redLimit = 9;
+    curlies.fileExtension = @"m";
+
+    NSArray *recipes = @[curlies,curlies];
+
+    return recipes;
 }
 
 
-- (NSView *)tableView:(NSTableView *)tableView
-   viewForTableColumn:(NSTableColumn *)tableColumn
-                  row:(NSInteger)row {
+#pragma mark - JWCTableViewDataSource, JWCTableViewDelegate
 
-    // Get an existing cell with the MyView identifier if it exists
-    NSTextField *result = [tableView makeViewWithIdentifier:@"MyView" owner:self];
 
-    // There is no existing cell to reuse so create a new one
-    if (result == nil) {
-
-        result = [[NSTextField alloc] initWithFrame:CGRectMake(0, 0, 200, 30)];
-        result.backgroundColor = [NSColor clearColor];
-        result.bordered = NO;
-        result.identifier = @"MyView";
-    }
-
-    SDWRecipe *recipe = self.items[row];
-
-    result.stringValue = recipe.name;
-
-    return result;
-    
+-(BOOL)tableView:(NSTableView *)tableView shouldSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Selected section %ld, row %ld",(long)indexPath.section,(long)indexPath.row);
+    return YES;
 }
 
-- (CGFloat)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row {
+-(BOOL)tableView:(NSTableView *)tableView shouldSelectSection:(NSInteger)section
+{
+    NSLog(@"Selected section header for section %ld", (long)section);
+    return YES;
+}
+
+
+
+//Number of rows in section
+-(NSInteger)tableView:(NSTableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    NSString *key = [[self sectionKeys] objectAtIndex:section];
+    NSArray *contents = [[self sectionContents] objectForKey:key];
+    NSInteger rows = [contents count];
+
+    return rows;
+
+}
+
+//Number of sections
+-(NSInteger)numberOfSectionsInTableView:(NSTableView *)tableView {
+    NSInteger sections = [[self sectionKeys] count];
+    return sections;
+}
+
+//Has a header view for a section
+-(BOOL)tableView:(NSTableView *)tableView hasHeaderViewForSection:(NSInteger)section {
+    return YES;
+}
+
+//Height related
+-(CGFloat)tableView:(NSTableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+
     return 60;
 }
 
-//- (NSTableRowView *)tableView:(NSTableView *)tableView rowViewForRow:(NSInteger)row {
-//
-//    NSTableRowView *r = [[NSTableRowView alloc]initWithFrame:CGRectMake(0, 0, 200, 60)];
-//    return r;
-//
-//}
-//
-//- (void)tableView:(NSTableView *)tableView didAddRowView:(NSTableRowView *)rowView forRow:(NSInteger)row {
-//
-//}
+-(CGFloat)tableView:(NSTableView *)tableView heightForHeaderViewForSection:(NSInteger)section {
+
+    return 30;
+}
+
+
+-(NSView *)tableView:(NSTableView *)tableView viewForHeaderInSection:(NSInteger)section {
+
+    NSTableCellView *resultView = [tableView makeViewWithIdentifier:@"cellView" owner:self];
+
+    resultView.textField.stringValue = [[self sectionKeys] objectAtIndex:section];
+    resultView.textField.textColor = [NSColor blackColor];
+
+    return resultView;
+}
+
+-(NSView *)tableView:(NSTableView *)tableView viewForIndexPath:(NSIndexPath *)indexPath {
+
+
+    NSString *key = [[self sectionKeys] objectAtIndex:[indexPath section]];
+    NSArray *contents = [[self sectionContents] objectForKey:key];
+    SDWRecipe *recipe = [contents objectAtIndex:[indexPath row]];
+
+
+    NSTableCellView *resultView = [tableView makeViewWithIdentifier:@"cellView" owner:self];
+    resultView.textField.stringValue = recipe.name;
+    resultView.textField.textColor = [NSColor darkGrayColor];
+
+    return resultView;
+}
 
 @end
